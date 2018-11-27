1f10ef5d8bf7c1a703ce507d22e26ea570d781d3
Yehuda Hermawan <3103117336@student.smktelkom-pwt.sch.id>
2018-11-27 12:26:02 +0700

    update delete

--- index.php
+++ index.php
@@ -11,7 +11,11 @@
 	<table align="center" border="2" width="70%">
 		<tr>
 			<td colspan="5">
-				<h3><center>DATA</center></h3>
+				<h3><center>DATA PARTISIPAN</center></h3>
+			</td>
+			<td>
+				<h3><center>Ubah</center></h3>
+				<a href="insert.php"><center>Tambah</center></a>
 			</td>
 		</tr>
 		<tr>
@@ -33,6 +37,11 @@
 			<td><?php echo $data['password']; ?></td>
 			<td><?php echo $data['level']; ?></td>
             <td><?php echo $data['fullname']; ?></td>
+            <td>
+			<a onclick="return confirm('Yakin untuk menghapus ?')"
+			href="delete.php?id=<?php echo $data['id']; ?>">Hapus</a>
+			<a href="update.php?id=<?php echo $data['id']; ?>">Edit</a>
+			</td>
 			
 		</tr>
 		
